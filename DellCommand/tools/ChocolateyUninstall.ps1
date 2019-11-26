87e152b4609b369e7127a805e12be9f8d0b65a6e
Adam Gell <adam@nextstepta.com>
2019-11-26 14:28:58 -0500

    changes

---
+++ DellCommand/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for EXE installations
+
+<#! PRE-UNINSTALL-TASKS !#>
+
+Uninstall-ChocolateyPackage -PackageName 'Citrix-Workspace' -FileType 'EXE' -SilentArgs '/silent /uninstall /cleanup' -File "$env:ALLUSERSPROFILE\Citrix\Citrix Workspace 1904\TrolleyExpress.exe" -ValidExitCodes @(0,3010);
+
+<#! POST-UNINSTALL-TASKS !#>
